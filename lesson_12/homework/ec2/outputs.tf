3ea36aea5aa5a33f577202ebc379efffc73835d5
gladkyi <andriigladkyi@icloud.com>
2025-11-12 18:45:57 +0100

    homework

--- lesson_12/homework/ec2/outputs.tf
+++ lesson_12/homework/ec2/outputs.tf
@@ -1,9 +1,9 @@
-# output "instance_ids" {
-#   description = "Launched EC2 instance IDs"
-#   value       = [for i in aws_instance.my_test_t3_micro : i.id]
-# }
+output "instance_ids" {
+  description = "Launched EC2 instance IDs"
+  value       = [for i in aws_instance.my_test_t3_micro : i.id]
+}
 
-# output "public_ips" {
-#   description = "Public IP addresses of the EC2 instances"
-#   value       = [for i in aws_instance.my_test_t3_micro : i.public_ip]
-# }
+output "public_ips" {
+  description = "Public IP addresses of the EC2 instances"
+  value       = [for i in aws_instance.my_test_t3_micro : i.public_ip]
+}
